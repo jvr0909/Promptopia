598082c377934251a4d706a03405b1c49ccda9c2
Jai <jrajpal2@illinois.edu>
2023-05-31 19:56:30 -0400

    fixed caching

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,17 +1,14 @@
-import {connectToDB} from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
     try {
-        await connectToDB();
+        await connectToDB()
 
-        const prompts = await Prompt.find({}).populate("creator");
+        const prompts = await Prompt.find({}).populate('creator')
 
-        return new Response(JSON.stringify(prompts), {status: 200});
-
+        return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
-        console.log("Error from GET /api/prompt: ", error);
-        return new Response("Failed to get prompts!", {status: 500});
-            
+        return new Response("Failed to fetch all prompts", { status: 500 })
     }
-}
+} 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -49,7 +49,12 @@ const Feed = () => {
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch('/api/prompt')
+            const response = await fetch('/api/prompt', {
+                method: 'GET',
+                headers: {
+                    'Cache-Control': 'no-store'
+                }
+            })
             const data = await response.json()
 
             setAllPosts(data)
