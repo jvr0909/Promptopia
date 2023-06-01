d9c86e05a345b341761389fc30108b94de8a6e21
Jai <jrajpal2@illinois.edu>
2023-06-01 00:46:21 -0400

    fixed caching

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -52,7 +52,7 @@ const Feed = () => {
             const response = await fetch('/api/prompt', {
                 method: 'GET',
                 headers: {
-                    'Cache-Control': 'no-store'
+                    'Cache-Control': 'private, no-cache, no-store, must-revalidate'
                 },
                 cache: 'no-store'
             })
